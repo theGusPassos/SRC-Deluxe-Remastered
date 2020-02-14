1e6c9b97257218fd4225044d1124315a14f842bc
Gustavo Passos <gustavo.passos@lambda3.com.br>
2020-02-14 19:18:24 -0300

    add basic input handler

---
+++ Assets/Scripts/Controller/CommandInterface.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controller
+{
+    public static class CommandInterface
+    {
+        public static float GetHorizontalInput()
+        {
+            return Input.GetAxisRaw("Horizontal");
+        }
+
+        public static float GetVerticalInput()
+        {
+            return Input.GetAxisRaw("Vertical");
+        }
+    }
+}
---
+++ Assets/Scripts/Controller/InputHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controller
+{
+    public class InputHandler : MonoBehaviour
+    {
+        private void Update()
+        {
+            float horizontalInput = CommandInterface.GetHorizontalInput();
+            float verticalInput = CommandInterface.GetVerticalInput();
+        }
+    }
+}
