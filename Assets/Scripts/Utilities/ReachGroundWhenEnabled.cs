cf04dbdd10dae15740483cadd26fec0d0d7990c5
Gustavo Passos <gustavo.passos@lambda3.com.br>
2020-02-16 13:11:51 -0300

    add checkpoint instantiator

---
+++ Assets/Scripts/Utilities/ReachGroundWhenEnabled.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utilities
+{
+    public class ReachGroundWhenEnabled : MonoBehaviour
+    {
+        [SerializeField] private LayerMask floorLayerMask;
+        [SerializeField] private float maxRayDistance;
+
+        private void OnEnable()
+        {
+            var meshRenderer = GetComponent<MeshRenderer>();
+
+            var ray = new Ray(transform.position, Vector3.down);
+            var hit = Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: maxRayDistance, layerMask: floorLayerMask);
+            if (hit)
+            {
+                var distanceFromGround = -hitInfo.distance + (meshRenderer.bounds.size.y / 2);
+                transform.position += new Vector3(0, distanceFromGround);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/RotateObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utilities
+{
+    public class RotateObject : MonoBehaviour
+    {
+        [SerializeField] private Vector3 rotation;
+
+        private void Update()
+        {
+            transform.Rotate(rotation * Time.deltaTime);
+        }
+    }
+}
