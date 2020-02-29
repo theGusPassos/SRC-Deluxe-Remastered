aa14da2e23c38cf98a7bb71cc08e914c1bfdc064
Gustavo Passos <theguspassos@gmail.com>
2020-02-29 15:34:10 -0300

    configure hp ui follower

--- Assets/Scripts/Battle/Health/HealthPoints.cs
+++ Assets/Scripts/Battle/Health/HealthPoints.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Battle.Health;
+using Assets.Scripts.Utilities;
+using UnityEngine;
 
 namespace Assets.Scripts.Battl.Health
 {
     public class HealthPoints : MonoBehaviour
     {
+        [SerializeField] private GameObject healthPointsUiPrefab;
         [SerializeField] private float maxHealthPoints;
         private float currentHealthPoints;
 
         private void Awake()
         {
             currentHealthPoints = maxHealthPoints;
+        }
+
+        private void Start()
+        {
+            var hpUiObject = Instantiate(healthPointsUiPrefab);
+            hpUiObject.name = $"Health Points Ui {gameObject.name}";
+
+            var objectFollower = hpUiObject.GetComponent<ObjectFollower>();
+            var hpUi = hpUiObject.GetComponent<HealthPointsUi>();
+
+            objectFollower.SetObjectToFollow(transform, false);
+            hpUi.SetHpPercentage(100);
         }
 
         public void DealDamage(float damage)
