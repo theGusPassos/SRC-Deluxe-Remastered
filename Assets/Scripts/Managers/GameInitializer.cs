18aedeb728a7ce0b4b319470334a0661b52e9b36
Gustavo Passos <theguspassos@gmail.com>
2020-04-21 16:58:50 -0300

    make game initializer use battle controller

--- Assets/Scripts/Managers/GameInitializer.cs
+++ Assets/Scripts/Managers/GameInitializer.cs
@@ -1,5 +1,4 @@
 ﻿using Assets.Scripts.Cars;
-using Assets.Scripts.Controller;
 using Assets.Scripts.Managers.Starter;
 using Assets.Scripts.Systems.Observable;
 using Assets.Scripts.UI.Battle;
@@ -8,34 +7,37 @@ using UnityEngine;
 namespace Assets.Scripts.Managers
 {
     [RequireComponent(typeof(InputHandlerCreator))]
-    [RequireComponent(typeof(CountDown))]
-    public class GameInitializer : MonoBehaviour, IEventListener<CountDownEvent>
+    [RequireComponent(typeof(BattleController))]
+    public class GameInitializer : MonoBehaviour, IEventListener<BattleEvent>
     {
         [SerializeField] private AvailableCars availableCars;
         [SerializeField] private CarPlacer carPlacer;
         [SerializeField] private CountDownUi countDownUi;
         private InputHandlerCreator inputHandlerCreator;
-        private CountDown countDown;
+        private BattleController battleController;
 
-        private CarController[] controllers;
+        private GameObject[] carsInstantiated;
 
         private void Awake()
         {
             inputHandlerCreator = GetComponent<InputHandlerCreator>();
-            countDown = GetComponent<CountDown>();
+            battleController = GetComponent<BattleController>();
+
+            battleController.SubscribeToEvent(this);
         }
 
         public void StartGame(int[] carsFromPlayers)
         {
             var carsPrefab = availableCars.GetCarsById(carsFromPlayers);
-            var carsInstantiated = InstantiateCars(carsPrefab);
+            carsInstantiated = InstantiateCars(carsPrefab);
+            inputHandlerCreator.CreateInputHandlers(carsInstantiated);
 
-            inputHandlerCreator.CreateInputHandlers(carsInGame);
-            SetControllers(carsInGame);
+            StartBattle();
+        }
 
-            SetCarsCountDownState(true);
-            countDown.SubscribeToEvent(this, countDownUi);
-            countDown.StartCountDown();
+        private void StartBattle()
+        {
+            battleController.StartBattle(carsInstantiated);
         }
 
         private GameObject[] InstantiateCars(GameObject[] carsPrefabs)
@@ -48,26 +50,11 @@ namespace Assets.Scripts.Managers
             return carsInstantiated;
         }
 
-        private void SetControllers(GameObject[] carsInGame)
+        public void SendEvent(BattleEvent e)
         {
-            controllers = new CarController[carsInGame.Length];
-            for (int i = 0; i < carsInGame.Length; i++)
-                controllers[i] = carsInGame[i].GetComponent<CarController>();
-        }
-
-        public void SendEvent(CountDownEvent e)
-        {
-            if (e == CountDownEvent.ENDED)
+            if (e == BattleEvent.ENDED)
             {
-                SetCarsCountDownState(false);
-            }
-        }
-
-        private void SetCarsCountDownState(bool waiting)
-        {
-            foreach (var controller in controllers)
-            {
-                controller.State.WaitingStartCountDown = waiting;
+                Debug.LogError("this battle has ended");
             }
         }
     }
