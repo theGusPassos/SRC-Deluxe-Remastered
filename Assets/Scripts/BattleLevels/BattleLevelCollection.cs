fa2776383732a40d84bdce2e444481f68e0df75f
Gustavo Passos <theguspassos@gmail.com>
2020-04-22 20:15:45 -0300

    add battle level collection with scene ifno

---
+++ Assets/Scripts/BattleLevels/BattleLevelCollection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.BattleLevels
+{
+    public class BattleLevelCollection
+    {
+        [SerializeField] private BattleLevel[] battleLevels;
+        private int currentLevel = 0;
+
+        public void GoToFirstLevel() => currentLevel = 0;
+
+        public void GoToNextLevel()
+        {
+            currentLevel++;
+        }
+
+        public string GetCurrentLevelScene() => battleLevels[currentLevel].sceneName;
+
+        public bool IsInLastLevel() => currentLevel >= battleLevels.Length;
+    }
+
+    [System.Serializable]
+    public struct BattleLevel
+    {
+        [SerializeField] public string sceneName;
+    }
+}
